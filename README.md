b02d74cb0f353468c6c1e3c11175d51b5a846853
Shreyas <shreyas.global.7@gmail.com>
2026-08-03 17:54:12 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Python-Concepts
-This repo is intended to share, the fundamental core concepts of python.
---
+++ variables/main.py
@@ -0,0 +1,28 @@
+import keyword
+
+
+"""
+Variable Identifier Rules:
+
+1 - The varibale name must not be a keyword.
+2 - The variable must not start with any digit or number.
+3 - The varibale name must not contain any space.
+4 - The varibale name must not contain any special character except '_'.
+5 - A variable is alphanumeric entity, that too starting with alphabet only.
+"""
+
+
+"""
+A varibale name cannot be same as any element in the below list ->
+Run the print statement to see the list.
+"""
+print(keyword.kwlist)
+
+# Valid variable names.
+
+"""
+'str' is datatype of the object, that will be store to this variable inside memory.
+This method is called type hinting or type annotation.
+"""
+some_var: str = ''
+some_var_2: str = ''
