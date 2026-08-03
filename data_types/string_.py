2b7fed3726f28e07351f9fa79b59f2a1fdd7d205
Shreyas <shreyas.global.7@gmail.com>
2026-08-03 19:42:16 +0530

    Merge remote changes

--- data_types/string_.py
+++ data_types/string_.py
@@ -1,8 +1,6 @@
-"""
-Strings are immutable data type, 
-that said it cannot be edited / manipulated, in any way
-after creation.
-"""
+"Strings are immutable, "
+"so their contents cannot be changed in place after creation. "
+"String methods return new strings or other new objects instead."
 
 
 # Below is a demo of creating a string type data type.
@@ -11,3 +9,28 @@ random_string: str = "" # Default value [whose bool(var_name) is False always]
 random_string_2: str = "non-default" # Non-Defualt value [whose bool(var_name) is True always]
 
 print(bool(random_string), bool(random_string_2))
+
+# Dir function lets you know the applicable functions for that dtype.
+
+print(dir(str))
+"""
+['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__',
+ '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__',
+   '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__',
+     '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__',
+       '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__',
+         '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center',
+           'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map',
+             'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit',
+               'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace',
+                 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans',
+                   'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind',
+                     'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split',
+                       'splitlines', 'startswith', 'strip', 'swapcase', 'title',
+                         'translate', 'upper', 'zfill']"""
+
+random_string_3: str = random_string_2.strip('t')
+print(random_string_3)
+
+# 2886770282480 2886770287920 -> both strings have different id's, not same object.
+print(id(random_string_2), id(random_string_3))
