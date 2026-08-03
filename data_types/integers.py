3611a0992110fb895612091b8de1950d00c5c6e2
Shreyas <shreyas.global.7@gmail.com>
2026-08-03 19:10:13 +0530

    Merge remote changes

---
+++ data_types/integers.py
@@ -0,0 +1,11 @@
+"""
+Integers are immutable data type, 
+that said it cannot be edited / manipulated, in any way
+after creation.
+"""
+
+
+# Below is a demo of creating a integer type data type.
+
+random_int: int = 0 # Default value [whose bool(var_name) is False always]
+random_int_2: int = 10 # Non-Defualt value [whose bool(var_name) is True always]
