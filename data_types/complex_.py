9cd7c11c230718be9c16434642531b427d5b482e
Shreyas <shreyas.global.7@gmail.com>
2026-08-03 19:23:11 +0530

    Merge remote changes

---
+++ data_types/complex_.py
@@ -0,0 +1,12 @@
+"""
+Complex numbers are immutable data type, 
+that said it cannot be edited / manipulated, in any way
+after creation.
+"""
+
+
+# Below is a demo of creating a complex type data type.
+# j=√-1
+
+random_complex: complex = 0j # Default value [whose bool(var_name) is False always]
+random_complex_2: complex = 10+9j # Non-Defualt value [whose bool(var_name) is True always]
--- data_types/float_.py
+++ data_types/float_.py
@@ -5,7 +5,7 @@ after creation.
 """
 
 
-# Below is a demo of creating a integer type data type.
+# Below is a demo of creating a float type data type.
 
 random_float: float = 0 # Default value [whose bool(var_name) is False always]
 random_float_2: float = 10.12 # Non-Defualt value [whose bool(var_name) is True always]
