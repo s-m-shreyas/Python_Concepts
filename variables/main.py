a83ac521183246aec6a96d645298018138f840b9
Shreyas <shreyas.global.7@gmail.com>
2026-08-03 19:01:32 +0530

    rnd

--- variables/main.py
+++ variables/main.py
@@ -24,7 +24,17 @@ print(keyword.kwlist)
 'str' is datatype of the object, that will be store to this variable inside memory.
 This method is called type hinting or type annotation.
 """
-some_var: str = ''
-some_var_2: str = ''
-some_var_3, some_var_4 = '', ''
+def main()->None:
+
+    some_var: str = ''
+    some_var_2: str = ''
+    some_var_3, some_var_4 = '', ''
+    print(some_var,
+          some_var_2,
+          some_var_3,
+          some_var_4)
+
+
+if __name__ == '__main__':
+    main()
 
