5c005b2bb3b7643cff1cf6ec9c81151256fab845
Shreyas <shreyas.global.7@gmail.com>
2026-08-03 19:16:30 +0530

    Merge remote changes

---
+++ data_types/float_.py
@@ -0,0 +1,11 @@
+"""
+Floats are immutable data type, 
+that said it cannot be edited / manipulated, in any way
+after creation.
+"""
+
+
+# Below is a demo of creating a integer type data type.
+
+random_float: float = 0 # Default value [whose bool(var_name) is False always]
+random_float_2: float = 10.12 # Non-Defualt value [whose bool(var_name) is True always]
