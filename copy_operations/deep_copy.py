a9b98f22f368b7df51adc01debe58c35da3f5c98
Shreyas <shreyas.global.7@gmail.com>
2026-08-05 12:35:29 +0530

    Merge remote changes

--- copy_operations/deep_copy.py
+++ copy_operations/deep_copy.py
@@ -3,24 +3,24 @@
 from typing import cast
 from copy import deepcopy
 
-list_c: list[int|list[int]] = [1, 2, 3, 4, 5, [1, 2, 3]]
-list_d: list[int|list[int]] = deepcopy(list_c)
+list_a: list[int|list[int]] = [1, 2, 3, 4, 5, [1, 2, 3]]
+list_b: list[int|list[int]] = deepcopy(list_a)
 
 # Id of both variables will not be same.
-print(id(list_c), id(list_d)) # 2578470584512 2578473526464
+print(id(list_a), id(list_b)) # 2578470584512 2578473526464
 
-# changes made to list_d will not be applied to list_c as well.
-list_d.append(6)
+# changes made to list_b will not be applied to list_a as well.
+list_b.append(6)
 # [1, 2, 3, 4, 5, [1, 2, 3]] [1, 2, 3, 4, 5, [1, 2, 3], 6]
-print(list_c, list_d)
+print(list_a, list_b)
 
 # just to satisfy Mypy
-cast(list[int], list_d[5])[0] = 0
+cast(list[int], list_b[5])[0] = 0
 
 """
 changes made to 
-nested list of list_d will not affect the 
-nested list of list_c
+nested list of list_b will not affect the 
+nested list of list_a
 """
 # [1, 2, 3, 4, 5, [1, 2, 3]] [1, 2, 3, 4, 5, [0, 2, 3], 6]
-print(list_c, list_d)
+print(list_a, list_b)
--- copy_operations/shallow_copy.py
+++ copy_operations/shallow_copy.py
@@ -2,25 +2,25 @@
 
 from typing import cast
 
-list_c: list[int|list[int]] = [1, 2, 3, 4, 5, [1, 2, 3]]
-list_d: list[int|list[int]] = list_c.copy()
+list_a: list[int|list[int]] = [1, 2, 3, 4, 5, [1, 2, 3]]
+list_b: list[int|list[int]] = list_a.copy()
 
 # Id of both variables will not be same.
-print(id(list_c), id(list_d)) # 1550178521664 1550178566336
+print(id(list_a), id(list_b)) # 1550178521664 1550178566336
 
-# changes made to list_d will not be applied to list_c as well.
-list_d.append(6)
+# changes made to list_b will not be applied to list_a as well.
+list_b.append(6)
 # [1, 2, 3, 4, 5, [1, 2, 3]] [1, 2, 3, 4, 5, [1, 2, 3], 6]
-print(list_c, list_d)
+print(list_a, list_b)
 
 # just to satisfy Mypy
-cast(list[int], list_d[5])[0] = 0
+cast(list[int], list_b[5])[0] = 0
 
 """
 But changes made to 
-nested list of list_d will affect the 
-nested list of list_c
+nested list of list_b will affect the 
+nested list of list_a
 """
 # [1, 2, 3, 4, 5, [0, 2, 3]] [1, 2, 3, 4, 5, [0, 2, 3], 6]
-print(list_c, list_d)
+print(list_a, list_b)
 
