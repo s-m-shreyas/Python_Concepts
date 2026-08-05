0f4a64ed6975c9b5105443c3f0947520b933193d
Shreyas <shreyas.global.7@gmail.com>
2026-08-05 12:07:12 +0530

    Merge remote changes

--- copy_operations/deep_copy.py
+++ copy_operations/deep_copy.py
@@ -1,10 +1,10 @@
 # Example:
 
 from typing import cast
-import copy
+from copy import deepcopy
 
 list_c: list[int|list[int]] = [1, 2, 3, 4, 5, [1, 2, 3]]
-list_d: list[int|list[int]] = copy.deepcopy(list_c)
+list_d: list[int|list[int]] = deepcopy(list_c)
 
 # Id of both variables will not be same.
 print(id(list_c), id(list_d)) # 2578470584512 2578473526464
@@ -22,5 +22,5 @@ changes made to
 nested list of list_d will not affect the 
 nested list of list_c
 """
-# [1, 2, 3, 4, 5, [0, 2, 3]] [1, 2, 3, 4, 5, [0, 2, 3], 6]
+# [1, 2, 3, 4, 5, [1, 2, 3]] [1, 2, 3, 4, 5, [0, 2, 3], 6]
 print(list_c, list_d)
